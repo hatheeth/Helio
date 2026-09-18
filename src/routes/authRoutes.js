49ff3f15bc7944e328f73fde541898e597d3b6b1
hatheeth <hathithinsan02@outlook.com>
2026-09-18 19:03:52 +0530

    Auth testing4

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,11 +1,11 @@
-import authController from "../controller/authController.js";
+import { signup, login } from '../controller/authController.js';
 import express from "express";
 
 
 const router = express.Router();
 
 
-router.post('/signup', authController.signup);
-router.post('/login', authController.login);
+router.post('/signup', signup);
+router.post('/login', login);
 
 export default router;
