3f6be3bfaf498e16ad284cea5ed4f7ce56fffc78
hatheeth <hathithinsan02@outlook.com>
2026-09-18 19:07:41 +0530

    Auth testing5

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,13 +1,13 @@
 import { supabase } from '../db/supabaseClient.js';
 
-exports.signup = async (req, res) => {
+export const signup = async (req, res) => {
   const { email, password } = req.body;
   const { data, error } = await supabase.auth.signUp({ email, password });
   if (error) return res.status(400).json({ error: error.message });
   res.json({ user: data.user });
 };
 
-exports.login = async (req, res) => {
+export const login = async (req, res) => {
   const { email, password } = req.body;
   const { data, error } = await supabase.auth.signInWithPassword({ email, password });
   if (error) return res.status(400).json({ error: error.message });
