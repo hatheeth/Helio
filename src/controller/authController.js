96e564a2f9f09c7cfd20f311017ffaeba83886a8
hatheeth <hathithinsan02@outlook.com>
2026-09-20 08:25:25 +0530

    testing insert

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,16 +1,42 @@
 import { supabase } from '../db/supabaseClient.js';
 
 export const signup = async (req, res) => {
-  const { email, password } = req.body;
-  const { data, error } = await supabase.auth.signUp({ email, password });
+  const { email, password, username } = req.body;
+
+  // Step 1: Create account in Supabase Auth
+  const { data, error } = await supabase.auth.signUp({
+    email,
+    password,
+    options: {
+      data: { username } 
+    }
+  });
+
   if (error) return res.status(400).json({ error: error.message });
-  res.json({ user: data.user });
+
+  const user = data.user;
+
+  
+  const { error: insertError } = await supabase
+    .from('users')
+    .insert([
+      {
+        id: user.id,              // UUID from auth.users
+        email: user.email,        // email
+        username: username        // custom field
+      }
+    ]);
+
+  if (insertError) return res.status(400).json({ error: insertError.message });
+
+  res.json({ user });
 };
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
+
   const { data, error } = await supabase.auth.signInWithPassword({ email, password });
   if (error) return res.status(400).json({ error: error.message });
+
   res.json({ session: data.session });
 };
-
