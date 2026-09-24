d0f4125f9a304cd4d4f37a8f7ec1fea82c888d4b
hatheeth <hathithinsan02@outlook.com>
2026-09-24 10:23:30 +0530

    logout1

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -36,17 +36,27 @@ export const signup = async (req, res) => {
   });
 };
 
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const { data, error } = await supabase.auth.signInWithPassword({ email, password });
+  if (error) return res.status(400).json({ error: error.message });
+
+  res.json({ session: data.session });
+};
+
 export const logout = async (req, res) => {
   try {
-    const { accessToken } = req.body; 
+    const { accessToken } = req.body;
 
-    
+
     const { data: { user }, error } = await supabase.auth.getUser(accessToken);
     if (error || !user) {
       return res.status(401).json({ error: "Invalid token" });
     }
 
-    
+
     await supabase.auth.admin.deleteRefreshTokens(user.id);
 
     return res.status(200).json({ message: "Logged out successfully" });
